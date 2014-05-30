0b2bb9b28099cc4d3dc2588a4d5c5212023e1e69
Andrew Baker <agb.baker@gmail.com>
2014-05-30 13:04:49 -0600

    push fourpoint repo to git. first update to remove jobs from employment section

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<!--[if lt IE 9 ]>    <html lang="en" class="no-js ie ie8"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html class='no-js' lang='en'>
+  <!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Private oil & gas exploration & production company – FourPoint Energy</title>
+	<meta name="description" content="Introducing FourPoint Energy, a private oil & gas exploration and production company founded by George Solich and the leadership of Cordillera Energy Partners">
+	<meta name="author" content="Karsh Hagan">  	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+	<link rel="stylesheet" href="css/style.css" />
+
+	<script type="text/javascript" src="/js/libs/modernizr.js"></script>
+	
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="/favicon.ico" type="image/x-icon">
+	
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	
+	  ga('create', 'UA-46816205-1', 'fourpointenergy.com');
+	  ga('send', 'pageview');
+	
+	</script>
+
+	</head>
+	<body>
+		
+		<header>
+			<div class="container">
+				<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" class="fourpoint_logo" alt="FourPoint Energy Logo">
+				<h1>After you build three successful companies in 12 years, the only thing left to do is start a fourth. </h1>
+				<h3>FourPoint Energy. A new energy company from the leadership team of Cordillera. </h3>
+			</div>
+			
+		</header>	
+		
+		<section class="container main">
+			<h2>About FourPoint Energy</h2>
+			<p>FourPoint Energy is a private exploration and production company founded by the leadership team of Cordillera Energy Partners following the sale to Apache Corporation in 2012. With over 30 years of industry-leading experience, FourPoint Energy will direct its initial focus to the core areas of the Anadarko and Permian basins. Stay tuned—our full website will be launching&nbsp;shortly. </p>
+			
+		</section>	
+		
+		<section class="main_cta">
+			<section class="container main">
+				<h2>Join Our Team</h2>
+				<p>We are currently hiring qualified individuals in the following areas: </p>
+			</section>	
+			<section class="container clearfix jobs">
+				<div>
+					<h4>Accounting</h4>
+					<ul>
+						<li>JIB Accountant</li>
+						<li>Contract Administrator</li>
+						<li>Marketing Representative</li>
+					</ul>
+				</div>
+				<div>						
+					<h4>Engineering</h4>
+					<ul>
+						<li>Reservoir Engineer</li>
+						<li>Operations Engineer</li>
+						<li>Production Technician</li>
+					</ul>
+				</div>
+				<div>
+					<h4>Land</h4>
+					<ul>
+						<li>Landman</li>
+						<li>Land Administration Analyst</li>
+					</ul>
+				</div>
+				<div>
+					<h4>Business Development</h4>
+					<ul>
+						<li>Business Development Analyst</li>
+					</ul>
+				</div>
+			</section>
+			<p>Please submit a resume and cover letter to <a href="mailto:jobs@fourpointenergy.com">jobs@fourpointenergy.com</a>. <br /><strong>Include the specific job title you're applying for in the subject line of the email.</strong></p>
+			<a href="mailto:jobs@fourpointenergy.com?subject=FourPoint Energy Job Inquiry" class="cta">Email Us</a>	
+		</section>
+		
+		<footer>
+			<section class="container main">
+				<p><strong>FourPoint Energy, LLC</strong></p>
+				<p>8450 E. Crescent Parkway, Suite 400, Greenwood Village, CO 80111</p>
+				<p class="call">303.290.0990</p>
+				<a class="mobile_call" href="tel:3032900990">303.290.0990</p>
+			</section>
+		</footer>
+			
+
+	</body>
+</html>
